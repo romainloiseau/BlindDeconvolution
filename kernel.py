a912db2ec275c2ef25bfa7096147914a1e85c3dd
romain.loiseau <romain.loiseau@polytechnique.edu>
2018-10-17 23:15:56 +0200

    optimize Toepliz

--- kernel.py
+++ kernel.py
@@ -6,7 +6,8 @@ Created on Wed Oct 17 19:24:47 2018
 """
 
 import numpy as np
-from scipy import ndimage
+from scipy import ndimage, sparse
+import time
 
 class Kernel:
     
@@ -24,25 +25,24 @@ class Kernel:
     
     def convolve2(self, matrix):
         N0, N1 = matrix.shape
-        T = self.getToepliz(N0, N1)
-        return np.dot(T, matrix.flatten()).reshape(N0 - self.getShape()[0] + 1, N1 - self.getShape()[1] + 1)
+        #return np.dot(T, matrix.flatten()).reshape(N0 - self.getShape()[0] + 1, N1 - self.getShape()[1] + 1)
+        return self.getToepliz(N0, N1).dot(matrix.flatten()).reshape(N0 - self.getShape()[0] + 1, N1 - self.getShape()[1] + 1)
     
     def getToepliz(self, N0, N1):
         N = N0*N1
-        k0, k1 = self.kernel.shape
-        T = np.zeros(((N0 - k0 + 1)* (N1 - k1 + 1), N))
+        k0, k1 = self.getShape()
+        k = self.kernel
+        T = sparse.lil_matrix(((N0 - k0 + 1)* (N1 - k1 + 1), N))
+        t = time.time()
         
-        for i in range(0, N0 - k0 + 1):
-            for j in range(0, N1 - k1 + 1):
-                row = np.zeros(N)
-                for k0i in range(k0):
-                    for k1j in range(k1):
-                        ind = N1 * (i + k0i) + (j + k1j)
+        for k0i in range(k0):
+            for k1j in range(k1):
+                kk = k[k0i, k1j]
+                for i in range(0, N0 - k0 + 1):
+                    for j in range(0, N1 - k1 + 1):
+                        T[(N1 - k1 + 1) * i + j, N1 * (i + k0i) + (j + k1j)] = kk
                         
-                        if(ind >= 0 and ind < N):
-                            row[ind] = self.kernel[k0i, k1j]
-                T[(N1 - k1 + 1) * i + j, :] = row
-                
+        print(time.time() - t)
         return T
     
 def runTests():
@@ -72,3 +72,19 @@ def runTests():
     print(T)
     print("Result = ")
     print(k.convolve2(matrix))
+    
+    print()    
+    k = Kernel([[10, 1], [.1, .01]])
+    N0, N1 = 1000, 300
+    matrix = np.arange(N0*N1).reshape(N0, N1)
+    
+    ntimes = 1
+    t = time.time()
+    [k.convolve2(matrix) for i in range(ntimes)]
+    print("Toepliz :", (time.time() - t) / ntimes, "secs")
+    
+    t = time.time()
+    [k.convolve(matrix) for i in range(ntimes)]
+    print("Scipy :", (time.time() - t) / ntimes, "secs")
+    
+runTests()
